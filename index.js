0cbb5ad7625de95bae3e3c28f1b291c9d5754c2d
jiohc <85597770+jiohc@users.noreply.github.com>
2021-10-06 14:54:29 -0700

    added questions

--- index.js
+++ index.js
@@ -7,12 +7,183 @@ const generateMarkdown = require('./utils/generateMarkdown');
 
 
 // TODO: Create an array of questions for user input
+const questions = [
+    {
+        type: 'input',
+        name: 'projectTitle',
+        message: 'What is your project title? (Required)',
+        validate: projectTitleInput => {
+            if (projectTitleInput) {
+                return true;
+            } else {
+                console.log('Please enter your project title!');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Provide a detailed description about your project. (Required)',
+        validate: descriptionInput => {
+            if (descriptionInput) {
+                return true;
+            } else {
+                console.log('Please provide a detailed description about your project.');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'confirm',
+        name: 'tableOfContents',
+        message: 'Would you like add a table of contents to make is easier for users to find what they need. (Optional)',
+    },
+    {
+        type: 'input',
+        name: 'confirmTableOfContents',
+        message: 'Please add a table of contents for your project',
+        when: ({ confirmTableOfContents }) => {
+            if (confirmTableOfContents) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'Provide a step-by-step description required to install your project. (Required)',
+        validate: installationInput => {
+            if (installationInput) {
+                return true;
+            } else {
+                console.log('Please provide a step-by-step description required to install your project.');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Provide instructions and examples for use. (Required)',
+        validate: usageInput => {
+            if (usageInput) {
+                return true;
+            } else {
+                console.log('Please provide instructions and examples for use.');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'credits',
+        message: 'List your collaborators, if any, with links to their GitHub profiles. (Required)',
+        validate: creditsInput => {
+            if (creditsInput) {
+                return true;
+            } else {
+                console.log('Please List your collaborators, if any, with links to their GitHub profiles.');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'What kind of license do you want to add to your project (Required)',
+        choices: ['MIT', 'APACHE', 'GPL', 'BSD', 'None']
+    }
+];
+
+
+const readmeExtra = [
+console.log(`
+===========================================================================================================
+The sections listed above are the minimum for a good README but also consider adding the following sections
+===========================================================================================================
+`),
+
+    {
+        type: 'confirm',
+        name: 'confirmBadges',
+        message: 'Would you like to select any bages?',
+        default: true
+    },
+    {
+        type: 'input',
+        name: 'badges',
+        message: 'Please select badges',
+        when: ({ confirmBadges }) => {
+            if (confirmBadges) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+    },
+    {
+        type: 'confirm',
+        name: 'confirmFeatures',
+        message: 'Would you like to list the features of your projects?',
+        default: true
+    },
+    {
+        type: 'input',
+        name: 'features',
+        message: 'Please list the features of your project',
+        when: ({ confirmFeatures }) => {
+            if (confirmFeatures) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+    },
+    {
+        type: 'confirm',
+        name: 'confirmContributing',
+        message: 'Would like other developers to contribute?',
+        default: true
+    },
+    {
+        type: 'input',
+        name: 'contributing',
+        message: 'Please add guidelines for how other developers can contribute',
+        when: ({ confirmContributing }) => {
+            if (confirmContributing) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+    },
+    {
+        type: 'confirm',
+        name: 'confirmTests',
+        message: 'If you wrote tests for your application, would you like provide examples on how to run them?',
+        default: true
+    },
+    {
+        type: 'input',
+        name: 'tests',
+        message: 'Please provide examples on how to run the tests for your application',
+        when: ({ confirmtests }) => {
+            if (confirmtests) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+    },
+];
 
     
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
-    
 }
 
 // TODO: Create a function to initialize app
